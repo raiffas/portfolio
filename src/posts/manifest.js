2c81e2bc7f26de7524d2eab6bb95742038d1f017
raiffas <raiffas@gmail.com>
2026-07-06 09:29:40 -0400

    new blog post

--- src/posts/manifest.js
+++ src/posts/manifest.js
@@ -1,6 +1,7 @@
 const posts = [
   { title: "Hello World", date: "2026-06-24", file: "hello-world.md", tags: ["about me"], hideDate: true },
   { title: "Can I coexist with fear?", date: "2026-06-25", file: "second-post.md", tags: ["fear"], hideDate: true },
+  { title: "Fighting the urge to give up", date: "2026-07-06", file: "second-post.md", tags: ["fear"], hideDate: true }
 ];
 
 export default posts;
