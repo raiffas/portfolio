197ed15c386522d5372e3aa5af0c6f7efbcaaddb
raiffas <raiffas@gmail.com>
2026-07-10 10:03:56 -0400

    new blog post

--- src/posts/manifest.js
+++ src/posts/manifest.js
@@ -1,7 +1,8 @@
 const posts = [
   { title: "Hello World", date: "2026-06-24", file: "hello-world.md", tags: ["about me"], hideDate: true },
   { title: "Can I coexist with fear?", date: "2026-06-25", file: "second-post.md", tags: ["fear"], hideDate: true },
-  { title: "Fighting the urge to give up", date: "2026-07-06", file: "second-post.md", tags: ["fear, Recurse Center"], hideDate: true }
+  { title: "Fighting the urge to give up", date: "2026-07-06", file: "losing-hope.md", tags: ["fear", "Recurse Center"], hideDate: true }, 
+  { title: "The journey of falling in love with dance (again)", date: "2026-07-10", file: "dance.md", tags: ["dance"], hideDate: true }
 ];
 
 export default posts;
